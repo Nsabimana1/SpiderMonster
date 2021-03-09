1c8e209f78cc4e7de2956e94392224a950f9c03c
Spencer <james.spencer.thompson@gmail.com>
2021-03-09 12:44:40 -0600

    Music found, creditted in Attributions Script

---
+++ SpiderMonstor/Assets/Scripts/Attributions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+    /*"8bit Dungeon Level" Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 4.0 License
+http://creativecommons.org/licenses/by/4.0/
+     * 
+     * 
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
