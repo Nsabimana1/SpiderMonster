a3ffd4a52cbc887670a1e4ed7ab8bb90f539d5b2
Spencer <james.spencer.thompson@gmail.com>
2021-03-10 09:43:29 -0600

    UI Elements Added, Font Added

--- SpiderMonstor/Assets/Scripts/Attributions.cs
+++ SpiderMonstor/Assets/Scripts/Attributions.cs
@@ -7,8 +7,8 @@ public class Attributions : MonoBehaviour
     /*"8bit Dungeon Level" Kevin MacLeod (incompetech.com)
 Licensed under Creative Commons: By Attribution 4.0 License
 http://creativecommons.org/licenses/by/4.0/
-     * 
-     * 
+     * https://www.behance.net/gallery/21677209/Marske-FREE-FONT
+     * https://positron.itch.io/steampunk-fantasy-ui-pack
      */
 
     // Start is called before the first frame update
