9e816544f782deb142eeaea1e2a7ff17145b9e53
Innocent Nsabimana <nsabimanaii@hendrix.edu>
2021-03-10 16:15:13 -0600

    added predator movement

---
+++ SpiderMonstor/Assets/Scripts/predetorMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class predetorMovement : MonoBehaviour
+{
+    // Derived from https://stuartspixelgames.com/2018/06/24/simple-2d-top-down-movement-unity-c/
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 5f;
+
+    private bool sliding = false;
+
+    public GameObject glowSprite;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+        //if (GameManager.Instance.power > 0)
+        //{
+        //    glowSprite.SetActive(true);
+        //}
+        //else
+        //{
+        //    glowSprite.SetActive(false);
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        if (!sliding)
+        {
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+    }
+
+
+    public void Slide(bool sliding)
+    {
+        this.sliding = sliding;
+    }
+}
